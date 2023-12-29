62d189c38dba590e7f28947c68b06c5bd775221b
imabg <abhaygoswami676@gmail.com>
2023-12-29 23:07:21 +0530

    Add: swagger documentation for controller methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @ApiOperation({ summary: 'Allow user to login' })
   async login(@Body() body: LoginDto) {
     return this.authService.validateUser(body.email, body.password);
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
   @Post('create')
+  @ApiOperation({ summary: 'User creation' })
   async create(@Body() body: CreateUserDto) {
     return this.userService.userOnboarding(body);
   }
